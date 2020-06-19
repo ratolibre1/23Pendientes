9fde39fee58aec171cfedcd7b3a1135d678ec716
Arturo Vial <arturo.vial@equifax.com>
2020-06-19 18:36:47 -0400

    v0.0.2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
+import { Todo } from '../interfaces/todo';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(private todoService: TodoService) {}
+  todos = [];
 
-  callTodo() {
+  constructor(private todoService: TodoService) {
+  }
+
+  async ngOnInit() {
+    await this.showTodos();
+  }
+
+  async addTodo() {
     console.log('Agregar');
-    this.todoService.createTodo({title: 'Test', desc: 'First tests', completed: false});
+    await this.todoService.createTodo({title: 'Test', desc: 'First tests', completed: false});
+    await this.showTodos();
   }
 
   async showTodos() {
     console.log('Mostrar');
-    const todos = await this.todoService.getTodos();
-    console.log(todos);
+    this.todos = await this.todoService.getTodos();
+    console.log(this.todos);
+  }
+
+  async completeTodo(todo: Todo) {
+    console.log(`Completar todo ${todo.id}`);
+    await this.todoService.completeTodo(todo);
   }
 }
